0d147e17b162f5b6c06a9156f437323bdec21db8
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2022-03-09 17:02:59 +0100

    feat: Add docutils.conf & Use html5
    Related: https://github.com/cpitclaudel/alectryon/pull/79

--- src/print.sh
+++ src/print.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-alectryon --frontend coq+rst "$1" --backend webpage --output-directory output && open output/`basename $1 .v`.html
+alectryon --html-dialect=html5 --frontend coq+rst "$1" --backend webpage --output-directory output && open output/`basename $1 .v`.html
